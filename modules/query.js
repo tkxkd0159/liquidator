d42d8069800ed6749475cca3f53063c78123dd94
JAESEUNG LEE <tkxkd0159@gmail.com>
2021-05-22 11:50:07 +0900

    Update query.js

--- modules/query.js
+++ modules/query.js
@@ -1,14 +1,15 @@
 import got from "got";
 
 const URL = "https://api.compound.finance/api/v2/account";
-const params = {
-  network: "mainnet",
-  "max_health[value]": "1.0",
-  page_number: 1,
-};
-const searchParams = new URLSearchParams(params);
 
 async function myRes(url) {
+  const params = {
+    network: "mainnet",
+    "max_health[value]": "1.0",
+    page_number: 1,
+  };
+  const searchParams = new URLSearchParams(params);
+
   const res = await got(url, { searchParams });
   return res.body;
 }
@@ -20,13 +21,7 @@ function getTotalEntry(resBody) {
   return totalNum;
 }
 
-myRes(URL, searchParams);
-let AccountResponse = await myRes(URL, searchParams);
+let AccountResponse = await myRes(URL);
 const totalNum = getTotalEntry(AccountResponse);
 
 export { totalNum };
-
-// const response = await got('https://api.compound.finance/api/v2/account?page_size=20&page_number=2')
-//   const response = await got(
-//     `https://api.compound.finance/api/v2/account?addresses[]=${addrList[0]}&addresses[]=${addrList[1]}`
-//   );
